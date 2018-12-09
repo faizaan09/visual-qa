21b8513933b62261d89f2ca36746bec81a8bdec6
Faizaan Charania <faizaancharania@gmail.com>
2018-12-08 21:17:08 -0500

    attention changes in enc_dec and created one word model

--- main_one_word.py
+++ main_one_word.py
@@ -14,8 +14,8 @@ import torch.utils.data
 from torch.optim.lr_scheduler import StepLR
 from torch.autograd import Variable
 from datetime import datetime
-from model import Encoder_attn, Decoder
-from metrics import filterOutput, maskedLoss, word_accuracy
+from models.single_world_model import VQAModel
+from metrics import filterOutput, maskedLoss, word_accuracy, getIndicesFromEmbedding
 from torchtext.data import TabularDataset, Field, Iterator
 from tensorboardX import SummaryWriter
 
@@ -42,18 +42,12 @@ def main(params):
     writer = SummaryWriter(output_dir)
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-    SOS_token = '<sos>'
-    EOS_token = '<eos>'
-    PAD_token = '<pad>'
-
     TEXT = Field(
         sequential=True,
         use_vocab=True,
         tokenize=tokenizer,
         lower=True,
-        batch_first=True,
-        init_token=SOS_token,
-        eos_token=EOS_token)
+        batch_first=True)
     # LABEL = Field(sequential=True, use_vocab=True, tokenize=tokenizer, is_target=True, batch_first=True, init_token='#', eos_token='$')
     IMG_IND = Field(sequential=False, use_vocab=False, batch_first=True)
 
@@ -84,10 +78,6 @@ def main(params):
     TEXT.build_vocab(train, vectors='glove.6B.100d')
     vocab = TEXT.vocab
 
-    PAD_token_ind = vocab.stoi[PAD_token]
-    SOS_token_ind = vocab.stoi[SOS_token]
-    EOS_token_ind = vocab.stoi[EOS_token]
-
     print("Creating Embedding from vocab vectors ..")
     txt_embed = nn.Embedding.from_pretrained(vocab.vectors)
     print("Text Embeddings are generated of size ", txt_embed.weight.size())
@@ -98,43 +88,27 @@ def main(params):
 
     img_embed = nn.Embedding.from_pretrained(torch.FloatTensor(img_embs))
 
-    print("Creating Encoder_attn ..")
-    encoder = Encoder_attn(img_embed, txt_embed, params)
-    print(encoder)
+    print("Creating VQAModel ..")
+    word_gen_model = VQAModel(img_embed, txt_embed, params)
+    print(word_gen_model)
 
-    print("Creating Decoder ..")
-    decoder = Decoder(txt_embed, params)
-    print(decoder)
-
-    criterion = torch.nn.PairwiseDistance(keepdim=False)
+    criterion = torch.nn.MSELoss()
     criterion.to(device)
-    encoder.to(device)
-    decoder.to(device)
+    word_gen_model.to(device)
 
-    ## [Completed] TODO(Jay) : Remove this check and use .to(device)
-    # if params['cuda']:
-    #     encoder.cuda()
-    #     decoder.cuda()
-    #     criterion.cuda()
+    optimizer = torch.optim.Adam(
+        word_gen_model.parameters(),
+        lr=params['lr'],
+        weight_decay=1e-5,
+        amsgrad=True)
 
-    encoder_optimizer = torch.optim.Adam(
-        encoder.parameters(), lr=params['lr'], weight_decay=1e-5, amsgrad=True)
-    decoder_optimizer = torch.optim.Adam(
-        decoder.parameters(), lr=params['lr'], weight_decay=1e-5, amsgrad=True)
-
-    # encoder_optimizer = torch.optim.Adagrad(
-    #     encoder.parameters(), lr=params['lr'], weight_decay=1e-5)
-    # decoder_optimizer = torch.optim.Adagrad(
-    #     decoder.parameters(), lr=params['lr'], weight_decay=1e-5)
+    # optimizer = torch.optim.Adagrad(
+    #     word_gen_model.parameters(), lr=params['lr'], weight_decay=1e-5)
 
     if params['use_checkpoint']:
         checkpoint = torch.load(params['enc_dec_model'])
-        encoder.load_state_dict(checkpoint['encoder_state_dict'])
-        decoder.load_state_dict(checkpoint['decoder_state_dict'])
-        encoder_optimizer.load_state_dict(
-            checkpoint['encoder_optimizer_state_dict'])
-        decoder_optimizer.load_state_dict(
-            checkpoint['decoder_optimizer_state_dict'])
+        word_gen_model.load_state_dict(checkpoint['encoder_state_dict'])
+        optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
 
     for epoch in range(params['niter']):
 
@@ -148,12 +122,10 @@ def main(params):
         for is_train in (True, False):
             print('Is Training: ', is_train)
             if is_train:
-                encoder.train()
-                decoder.train()
+                word_gen_model.train()
                 data_iter = train_iter
             else:
-                encoder.eval()
-                decoder.eval()
+                word_gen_model.eval()
                 data_iter = val_iter
 
             total_loss = 0
@@ -166,73 +138,34 @@ def main(params):
                     if len(row) < params['batch_size']:
                         continue
 
-                    encoder.zero_grad()
-                    decoder.zero_grad()
+                    word_gen_model.zero_grad()
 
                     ans, img_ind, question = row.ans, row.img_ind, row.question
-                    batch_size = params['batch_size']
 
-                    ## target_length-1 since we are not predicting SOS token
-                    target_length = ans.shape[1] - 1
-
-                    encoder.hidden = encoder.init_hidden(params)
+                    word_gen_model.hidden = word_gen_model.init_hidden(params)
 
                     ans = ans.to(device)
                     img_ind = img_ind.to(device)
                     question = question.to(device)
-                    encoder.hidden = (encoder.hidden[0].to(device),
-                                      encoder.hidden[1].to(device))
+                    word_gen_model.hidden = (
+                        word_gen_model.hidden[0].to(device),
+                        word_gen_model.hidden[1].to(device))
 
                     img_ind = Variable(img_ind)
                     question = Variable(question)
+
+                    encoder_output = word_gen_model(img_ind, question)
+
+                    pred_ind = getIndicesFromEmbedding(
+                        encoder_output, vocab.vectors.to(device))
+
                     ans_embed = txt_embed(ans)
+                    batch_loss = criterion(encoder_output, ans_embed)
 
-                    encoder_output = encoder(img_ind, question)
-
-                    decoder_input = ans_embed[:, 0].reshape(
-                        (batch_size, 1, -1))  ## (batch_size, 1) check again
-                    ans_embed = ans_embed[:, 1:]  ## removed the SOS token
-                    ans = ans[:, 1:]  ## removed the SOS token
-
-                    decoder_hidden = decoder.init_hidden(
-                        encoder_output, params)
-
-                    if params['cuda']:
-                        decoder_hidden = (decoder_hidden[0].cuda(),
-                                          decoder_hidden[1].cuda())
-
-                    outputs = torch.zeros(batch_size, target_length,
-                                          params['txt_emb_size'])
-
-                    ## [Completed] TODO(Jay) : remove the sos token from the ans and ans_embed before calc loss and acc
-                    for di in range(target_length - 1):
-                        decoder_output, decoder_hidden = decoder(
-                            decoder_input, decoder_hidden)
-
-                        ## TODO(Jay) : Detach the input from history
-                        decoder_input = decoder_output
-
-                        outputs[:, di, :] = decoder_output.reshape(
-                            batch_size, -1)
-
-                    filtered_labels, filtered_label_embeds, filtered_outputs = filterOutput(
-                        outputs.reshape(batch_size * target_length, -1),
-                        ans.reshape(batch_size * target_length, -1),
-                        ans_embed.reshape(batch_size * target_length, -1),
-                        PAD_token_ind)
-
-                    filtered_label_embeds = filtered_label_embeds.to(device)
-                    filtered_outputs = filtered_outputs.to(device)
-
-                    batch_loss = maskedLoss(filtered_label_embeds,
-                                            filtered_outputs, criterion)
-
-                    batch_acc = word_accuracy(filtered_outputs,
-                                              vocab.vectors.to(device),
-                                              filtered_labels)
+                    batch_acc = (pred_ind == ans).sum()
 
                     total_loss += batch_loss.item()
-                    total_acc += batch_acc
+                    total_acc += batch_acc.item()
 
                     if is_train:
                         if i % 1000 == 0:
@@ -242,24 +175,18 @@ def main(params):
                                    total_loss / i, total_acc / i))
 
                         batch_loss.backward()
-                        encoder_optimizer.step()
-                        decoder_optimizer.step()
+                        optimizer.step()
 
                 avg_loss = total_loss / len(data_iter)
                 avg_acc = total_acc / len(data_iter)
 
                 if is_train:
                     PATH = os.path.join(output_dir, 'enc_dec_model.pth')
-                    torch.save({
-                        'encoder_state_dict':
-                        encoder.state_dict(),
-                        'decoder_state_dict':
-                        decoder.state_dict(),
-                        'encoder_optimizer_state_dict':
-                        encoder_optimizer.state_dict(),
-                        'decoder_optimizer_state_dict':
-                        decoder_optimizer.state_dict(),
-                    }, PATH)
+                    torch.save(
+                        {
+                            'encoder_state_dict': word_gen_model.state_dict(),
+                            'optimizer_state_dict': optimizer.state_dict(),
+                        }, PATH)
 
                     writer.add_scalars('data', {
                         'train_loss': avg_loss,
@@ -283,9 +210,11 @@ if __name__ == "__main__":
 
     # input json
     parser.add_argument(
-        '--input_train', default='vqa_train.csv', help='input json file')
+        '--input_train',
+        default='vqa_one_word_train.csv',
+        help='input csv file')
     parser.add_argument(
-        '--input_test', default='vqa_test.csv', help='input json file')
+        '--input_test', default='vqa_one_word_test.csv', help='input csv file')
     parser.add_argument(
         '--mapping_file',
         default='image_index.pkl',
@@ -300,7 +229,7 @@ if __name__ == "__main__":
         help='Flag which states whether to use the previous Model checkpoint')
     parser.add_argument(
         '--enc_dec_model',
-        default='output/20181208_1949/enc_dec_model.pth',
+        default='output/20181208_0927/enc_dec_model.pth',
         help='Saved model path')
     parser.add_argument(
         '--dataroot', default='./data/', help='path to dataset')
@@ -328,7 +257,7 @@ if __name__ == "__main__":
     parser.add_argument(
         '--beta1', type=float, default=0.5, help='beta1 for adam. default=0.5')
     parser.add_argument(
-        '--cuda', default=True, action='store_true', help='enables cuda')
+        '--cuda', default=False, action='store_true', help='enables cuda')
     parser.add_argument(
         '--outf',
         default='./output/',
--- models/single_world_model.py
+++ models/single_world_model.py
@@ -4,60 +4,7 @@ import torch.nn as nn
 from torch.autograd import Variable
 
 
-class Model(nn.Module):
-    def __init__(self, params):
-        super(Model, self).__init__()
-
-        with open(params['image_embeddings'], 'rb') as f:
-            img_embs = pkl.load(f)['image_features']
-
-        self.img_features = nn.Embedding(img_embs.shape[0], img_embs.shape[1])
-
-        self.img_features.weight.data.copy_(torch.from_numpy(img_embs))
-        self.img_features.weight.requires_grad = False
-        # self.img_features = nn.Embedding.from_pretrained(img_embs)
-        self.text_embedding = nn.Embedding.from_pretrained(
-            params['vocab'].vectors)
-
-        self.parse_quest = nn.LSTM(
-            params['txt_emb_size'], params['txt_emb_size'], batch_first=True)
-        self.hidden = self.init_lstm_hidden(params)
-
-        self.classifier = nn.Sequential(
-            nn.Linear(params['img_feature_size'] + params['txt_emb_size'],
-                      2500),
-            nn.ReLU(True),
-            nn.Linear(2500, 1000)  #nums_ans = 1000
-        )
-
-    def init_lstm_hidden(self, params):
-        # Before we've done anything, we dont have any hidden state.
-        # Refer to the Pytorch documentation to see exactly
-        # why they have this dimensionality.
-        # The axes semantics are (num_layers, minibatch_size, hidden_dim)
-        return (Variable(
-            torch.zeros(1, params['batch_size'], params['txt_emb_size'])),
-                Variable(
-                    torch.zeros(1, params['batch_size'],
-                                params['txt_emb_size'])))
-
-    def forward(self, img, quest):
-        img_embedding = self.img_features(img)
-        token_embeddings = self.text_embedding(quest)
-        # token_embeddings = torch.cat((token_embeddings)).view(token_embeddings.shape[0], 1, -1)
-
-        token_lstm_output, self.hidden = self.parse_quest(
-            token_embeddings, self.hidden)
-
-        quest_embeddings = self.hidden[0][0]
-        quest_img_vector = torch.cat((img_embedding, quest_embeddings), 1)
-
-        answer = self.classifier(quest_img_vector)
-
-        return answer
-
-
-class Encoder_attn(nn.Module):
+class VQAModel(nn.Module):
     def __init__(self, img_embed, txt_embed, params):
         super(Encoder_attn, self).__init__()
 
@@ -103,9 +50,7 @@ class Encoder_attn(nn.Module):
         self.fusion = nn.Sequential(
             nn.Linear(1000, 2500), nn.LeakyReLU(inplace=True),
             nn.BatchNorm1d(2500), nn.Dropout(0.4),
-            nn.Linear(2500, params['txt_emb_size']),
-            nn.BatchNorm1d(params['txt_emb_size']), nn.Dropout(0.2),
-            nn.LeakyReLU(inplace=True))
+            nn.Linear(2500, params['txt_emb_size']))
 
     def init_lstm_hidden(self, params):
         # Before we've done anything, we dont have any hidden state.
@@ -161,31 +106,6 @@ class Encoder_attn(nn.Module):
         img_embedding = self.image_fc(img_embedding)
         quest_img_vector = torch.mul(img_embedding, quest_embedding)
         # quest_img_vector = torch.cat((img_embedding, quest_embedding), 1)
-        context = self.fusion(quest_img_vector)
+        ans_embed = self.fusion(quest_img_vector)
 
-        return context
-
-
-class Decoder(nn.Module):
-    def __init__(self, txt_embed, params):
-        super(Decoder, self).__init__()
-
-        # self.relu = torch.nn.ReLU()
-        # self.text_embedding = txt_embed
-        self.LSTM = nn.LSTM(
-            params['txt_emb_size'], params['txt_emb_size'], batch_first=True)
-        # self.hidden = encoder_output
-
-    def init_hidden(self, encoder_output, params):
-        return (encoder_output.reshape(
-            shape=(1, params['batch_size'], params['txt_emb_size'])),
-                torch.zeros(1, params['batch_size'], params['txt_emb_size']))
-
-    def forward(self, input, hidden):
-
-        # token_embeddings  = self.text_embedding(input)
-        token_embeddings = input
-        # token_embeddings = self.relu(token_embeddings)
-        next_word_embed, hidden = self.LSTM(token_embeddings, hidden)
-
-        return next_word_embed, hidden
+        return ans_embed
